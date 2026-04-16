492b2a6912bacbe1e162337e894aca85bb805772
emzz19 <mjgarcia06052005@gmail.com>
2026-04-16 21:39:20 +0800

    Add files via upload

---
+++ BYTeLog/BYTeLog/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace ByTeLog_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            textBox1 = new TextBox();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            textBox2 = new TextBox();
+            pictureBox2 = new PictureBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            panel1 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.GradientInactiveCaption;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Lucida Fax", 11F, FontStyle.Italic);
+            textBox1.Location = new Point(111, 384);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(336, 22);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Smart Student, Smart Tracking";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.GradientActiveCaption;
+            button1.FlatAppearance.BorderColor = Color.White;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            button1.Location = new Point(194, 523);
+            button1.Name = "button1";
+            button1.Size = new Size(212, 41);
+            button1.TabIndex = 1;
+            button1.Text = "Let's Get Started";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.GradientActiveCaption;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(134, 523);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(45, 42);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.GradientInactiveCaption;
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("Constantia", 30F, FontStyle.Bold);
+            textBox2.Location = new Point(67, 77);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(455, 62);
+            textBox2.TabIndex = 3;
+            textBox2.Text = "WELCOME!!";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(496, 18);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(382, 551);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 4;
+            pictureBox2.TabStop = false;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Sylfaen", 25.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(97, 209);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(370, 63);
+            textBox3.TabIndex = 5;
+            textBox3.Text = " Manage Payments & \r\n";
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Sylfaen", 25.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox4.Location = new Point(67, 288);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(434, 63);
+            textBox4.TabIndex = 6;
+            textBox4.Text = "Stay Updated on Events";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.GradientActiveCaption;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(pictureBox2);
+            panel1.Location = new Point(44, 34);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(915, 599);
+            panel1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.InactiveBorder;
+            ClientSize = new Size(1006, 673);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = " ";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private TextBox textBox2;
+        private PictureBox pictureBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Panel panel1;
+    }
+}
---
+++ BYTeLog/BYTeLog/Form1.cs
@@ -0,0 +1,20 @@
+namespace ByTeLog_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
