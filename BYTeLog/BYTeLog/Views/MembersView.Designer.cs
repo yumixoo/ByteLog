3c41e6ebfa526e7f4de9aa0084b76ddc48a0d92c
emzz19 <mjgarcia06052005@gmail.com>
2026-04-16 22:38:20 +0800

    membership commit1

--- BYTeLog/BYTeLog/Views/MembersView.Designer.cs
+++ BYTeLog/BYTeLog/Views/MembersView.Designer.cs
@@ -28,34 +28,233 @@
         /// </summary>
         private void InitializeComponent()
         {
-            bigLabel1 = new ReaLTaiizor.Controls.BigLabel();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            panel6 = new Panel();
+            panel7 = new Panel();
+            panel8 = new Panel();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            panel6.SuspendLayout();
+            panel7.SuspendLayout();
+            panel8.SuspendLayout();
             SuspendLayout();
             // 
-            // bigLabel1
+            // panel1
             // 
-            bigLabel1.AutoSize = true;
-            bigLabel1.BackColor = Color.Transparent;
-            bigLabel1.Font = new Font("Poppins", 25.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            bigLabel1.ForeColor = Color.Black;
-            bigLabel1.Location = new Point(302, 270);
-            bigLabel1.Name = "bigLabel1";
-            bigLabel1.Size = new Size(351, 74);
-            bigLabel1.TabIndex = 0;
-            bigLabel1.Text = "Members View";
+            panel1.Controls.Add(textBox1);
+            panel1.Location = new Point(20, 35);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(163, 111);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(textBox5);
+            panel2.Location = new Point(20, 162);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(462, 273);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(textBox8);
+            panel3.Controls.Add(textBox6);
+            panel3.Location = new Point(491, 162);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(340, 273);
+            panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(textBox7);
+            panel4.Location = new Point(20, 449);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(462, 125);
+            panel4.TabIndex = 3;
+            panel4.Paint += panel4_Paint;
+            // 
+            // panel5
+            // 
+            panel5.Location = new Point(491, 449);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(340, 125);
+            panel5.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            panel6.Controls.Add(textBox2);
+            panel6.Location = new Point(235, 35);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(163, 111);
+            panel6.TabIndex = 5;
+            // 
+            // panel7
+            // 
+            panel7.Controls.Add(textBox3);
+            panel7.Location = new Point(452, 35);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(163, 111);
+            panel7.TabIndex = 1;
+            // 
+            // panel8
+            // 
+            panel8.Controls.Add(textBox4);
+            panel8.Location = new Point(668, 35);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(163, 111);
+            panel8.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ActiveCaption;
+            textBox1.Font = new Font("Segoe UI", 6F);
+            textBox1.Location = new Point(19, 13);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 21);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Membership Status";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.ActiveCaption;
+            textBox2.Font = new Font("Segoe UI", 6F);
+            textBox2.Location = new Point(14, 13);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 21);
+            textBox2.TabIndex = 1;
+            textBox2.Text = "Outstanding Balance";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.ActiveCaption;
+            textBox3.Font = new Font("Segoe UI", 6F);
+            textBox3.Location = new Point(22, 13);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 21);
+            textBox3.TabIndex = 2;
+            textBox3.Text = "Activities Attended";
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.ActiveCaption;
+            textBox4.Font = new Font("Segoe UI", 6F);
+            textBox4.Location = new Point(21, 13);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 21);
+            textBox4.TabIndex = 3;
+            textBox4.Text = "Clearance Status";
+            textBox4.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = SystemColors.GradientActiveCaption;
+            textBox5.Font = new Font("Segoe UI", 7F);
+            textBox5.Location = new Point(3, 3);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(144, 23);
+            textBox5.TabIndex = 0;
+            textBox5.Text = "My Attendance Record";
+            textBox5.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            textBox6.BackColor = SystemColors.GradientActiveCaption;
+            textBox6.Font = new Font("Segoe UI", 7F);
+            textBox6.Location = new Point(3, 3);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(105, 23);
+            textBox6.TabIndex = 1;
+            textBox6.Text = "My Financials";
+            textBox6.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            textBox7.BackColor = SystemColors.GradientActiveCaption;
+            textBox7.Font = new Font("Segoe UI", 7F);
+            textBox7.Location = new Point(3, 3);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(144, 23);
+            textBox7.TabIndex = 2;
+            textBox7.Text = "Upcoming Activities";
+            textBox7.TextAlign = HorizontalAlignment.Center;
+            textBox7.TextChanged += textBox7_TextChanged;
+            // 
+            // textBox8
+            // 
+            textBox8.BackColor = SystemColors.GradientActiveCaption;
+            textBox8.Font = new Font("Segoe UI", 7F);
+            textBox8.Location = new Point(3, 176);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(121, 23);
+            textBox8.TabIndex = 2;
+            textBox8.Text = "Payment History";
+            textBox8.TextAlign = HorizontalAlignment.Center;
             // 
             // MembersView
             // 
-            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(bigLabel1);
+            Controls.Add(panel8);
+            Controls.Add(panel7);
+            Controls.Add(panel6);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             Name = "MembersView";
-            Size = new Size(955, 615);
+            Size = new Size(849, 586);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            panel7.ResumeLayout(false);
+            panel7.PerformLayout();
+            panel8.ResumeLayout(false);
+            panel8.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private ReaLTaiizor.Controls.BigLabel bigLabel1;
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
+        private Panel panel6;
+        private Panel panel7;
+        private Panel panel8;
+        private TextBox textBox1;
+        private TextBox textBox5;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
     }
 }
